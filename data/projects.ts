cc3d4dc54bad64a5a2b7269fe7b536a600009785
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2026-03-03 03:35:52 +0700

    update projects data

--- data/projects.ts
+++ data/projects.ts
@@ -5,8 +5,8 @@ export const projects = [
       "I developed a full-stack, location-based attendance system for FKIP with role-based access control, location validation for accurate attendance tracking, and full CRUD functionality, allowing seamless management of users and attendance records.",
     techStack: ["Next.js", "Express Js", "MySql"],
     image: "/images/projects/attendance_pwa.png",
-    github: "#",
-    liveDemo: "#",
+    github: "https://github.com/mauluddin12z/frontend-attendance-app-fkip-unsri",
+    liveDemo: "https://attendance-pwa.hidayatmauluddin.my.id",
   },
   {
     title: "Hambali Furniture",
@@ -14,8 +14,8 @@ export const projects = [
       "Created an e-commerce website with an admin dashboard to manage products and content, improving online visibility for a furniture store.",
     techStack: ["Next.js", "Express Js", "MySql"],
     image: "/images/projects/hambali_furniture.png",
-    github: "#",
-    liveDemo: "#",
+    github: "https://github.com/mauluddin12z/HAMBALI-FURNITURE-WEB-FRONTEND",
+    liveDemo: "https://hambali-furniture.hidayatmauluddin.my.id",
   },
   {
     title: "Point of Sale - Warung 19",
@@ -23,8 +23,8 @@ export const projects = [
       "Developed a POS web application that streamlined ordering and transaction processes for a local restaurant.",
     techStack: ["Next.js", "Express Js", "MySql"],
     image: "/images/projects/pos_warung19.png",
-    github: "#",
-    liveDemo: "#",
+    github: "https://github.com/mauluddin12z/POS_RESTO19_FRONTEND",
+    liveDemo: "https://pos-resto.hidayatmauluddin.my.id",
   },
   {
     title: "Salesperson website - Isuzu Sumatera Selatan",
@@ -32,8 +32,8 @@ export const projects = [
       "Built a product catalog website with admin features for product and content management, enabling digital product showcasing.",
     techStack: ["Codeigniter 13", "MySql"],
     image: "/images/projects/isuzu_sumsel.png",
-    github: "#",
-    liveDemo: "#",
+    github: "https://github.com/mauluddin12z/website-for-isuzu-sales-person",
+    liveDemo: "https://isuzu-sumsel.hidayatmauluddin.my.id",
   },
   {
     title: "Inventory System - FKIP Universitas Sriwijaya",
@@ -41,7 +41,7 @@ export const projects = [
       "Implemented an inventory management system for the Faculty of Teacher Training and Education, improving data recording and reporting processes.",
     techStack: ["Codeigniter 13", "MySql"],
     image: "/images/projects/sipb_fkip.png",
-    github: "#",
-    liveDemo: "#",
+    github: "https://github.com/mauluddin12z/SISTEM-INFORMASI-PERSEDIAAN-BARANG",
+    liveDemo: "https://sipb-fkip.hidayatmauluddin.my.id",
   },
 ];
