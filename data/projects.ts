aa2e5b53b443352e1d0c19a8e079e8289d591219
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2026-04-15 18:49:04 +0700

    update data

--- data/projects.ts
+++ data/projects.ts
@@ -1,84 +1,102 @@
 export const projects = [
-  {
-    title: "Attendance PWA - FKIP Universitas Sriwijaya",
-    description:
-      "I developed a full-stack, location-based attendance system for FKIP with role-based access control, location validation for accurate attendance tracking, and full CRUD functionality, allowing seamless management of users and attendance records.",
-    techStack: ["Next.js", "Express Js", "MySql"],
-    image: "/images/projects/attendance_pwa.png",
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/mauluddin12z/frontend-attendance-app-fkip-unsri",
-      },
-      {
-        label: "Backend",
-        url: "https://github.com/mauluddin12z/backend-attendance-app-fkip-unsri",
-      },
-    ],
-    liveDemo: "https://attendance-pwa.hidayatmauluddin.my.id",
-  },
-  {
-    title: "Hambali Furniture",
-    description:
-      "Created an e-commerce website with an admin dashboard to manage products and content, improving online visibility for a furniture store.",
-    techStack: ["Next.js", "Express Js", "MySql"],
-    image: "/images/projects/hambali_furniture.png",
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/mauluddin12z/HAMBALI-FURNITURE-WEB-FRONTEND",
-      },
-      {
-        label: "Backend",
-        url: "https://github.com/mauluddin12z/HAMBALI-FURNITURE-WEB-BACKEND",
-      },
-    ],
-    liveDemo: "https://hambali-furniture.hidayatmauluddin.my.id",
-  },
-  {
-    title: "Point of Sale - Warung 19",
-    description:
-      "Developed a POS web application that streamlined ordering and transaction processes for a local restaurant.",
-    techStack: ["Next.js", "Express Js", "MySql"],
-    image: "/images/projects/pos_warung19.png",
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/mauluddin12z/POS_RESTO19_FRONTEND",
-      },
-      {
-        label: "Backend",
-        url: "https://github.com/mauluddin12z/POS_RESTO19_BACKEND",
-      },
-    ],
-    liveDemo: "https://pos-resto.hidayatmauluddin.my.id",
-  },
-  {
-    title: "Salesperson website - Isuzu Sumatera Selatan",
-    description:
-      "Built a product catalog website with admin features for product and content management, enabling digital product showcasing.",
-    techStack: ["Codeigniter 13", "MySql"],
-    image: "/images/projects/isuzu_sumsel.png",
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/mauluddin12z/website-for-isuzu-sales-person",
-      },
-    ],
-    liveDemo: "https://isuzu-sumsel.hidayatmauluddin.my.id",
-  },
-  {
-    title: "Inventory System - FKIP Universitas Sriwijaya",
-    description:
-      "Implemented an inventory management system for the Faculty of Teacher Training and Education, improving data recording and reporting processes.",
-    techStack: ["Codeigniter 13", "MySql"],
-    image: "/images/projects/sipb_fkip.png",
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/mauluddin12z/SISTEM-INFORMASI-PERSEDIAAN-BARANG",
-      },
-    ],
-    liveDemo: "https://sipb-fkip.hidayatmauluddin.my.id",
-  },
+   {
+      title: "Attendance PWA - FKIP Universitas Sriwijaya",
+      description:
+         "I developed a full-stack, location-based attendance system for FKIP with role-based access control, location validation for accurate attendance tracking, and full CRUD functionality, allowing seamless management of users and attendance records.",
+      techStack: ["Next.js", "Express Js", "MySql"],
+      image: "/images/projects/attendance_pwa.png",
+      github: [
+         {
+            label: "Frontend",
+            url: "https://github.com/mauluddin12z/frontend-attendance-app-fkip-unsri",
+         },
+         {
+            label: "Backend",
+            url: "https://github.com/mauluddin12z/backend-attendance-app-fkip-unsri",
+         },
+      ],
+      liveDemo: "https://attendance-pwa.hidayatmauluddin.my.id",
+   },
+   {
+      title: "Hambali Furniture",
+      description:
+         "Created an e-commerce website with an admin dashboard to manage products and content, improving online visibility for a furniture store.",
+      techStack: ["Next.js", "Express Js", "MySql"],
+      image: "/images/projects/hambali_furniture.png",
+      github: [
+         {
+            label: "Frontend",
+            url: "https://github.com/mauluddin12z/HAMBALI-FURNITURE-WEB-FRONTEND",
+         },
+         {
+            label: "Backend",
+            url: "https://github.com/mauluddin12z/HAMBALI-FURNITURE-WEB-BACKEND",
+         },
+      ],
+      liveDemo: "https://hambali-furniture.hidayatmauluddin.my.id",
+   },
+   {
+      title: "Point of Sale - Warung 19",
+      description:
+         "Developed a POS web application that streamlined ordering and transaction processes for a local restaurant.",
+      techStack: ["Next.js", "Express Js", "MySql"],
+      image: "/images/projects/pos_warung19.png",
+      github: [
+         {
+            label: "Frontend",
+            url: "https://github.com/mauluddin12z/POS_RESTO19_FRONTEND",
+         },
+         {
+            label: "Backend",
+            url: "https://github.com/mauluddin12z/POS_RESTO19_BACKEND",
+         },
+      ],
+      liveDemo: "https://pos-resto.hidayatmauluddin.my.id",
+   },
+   {
+      title: "Salesperson website - Isuzu Sumatera Selatan",
+      description:
+         "Built a product catalog website with admin features for product and content management, enabling digital product showcasing.",
+      techStack: ["Codeigniter 13", "MySql"],
+      image: "/images/projects/isuzu_sumsel.png",
+      github: [
+         {
+            label: "Fullstack",
+            url: "https://github.com/mauluddin12z/website-for-isuzu-sales-person",
+         },
+      ],
+      liveDemo: "https://isuzu-sumsel.hidayatmauluddin.my.id",
+   },
+   {
+      title: "Inventory System - FKIP Universitas Sriwijaya",
+      description:
+         "Implemented an inventory management system for the Faculty of Teacher Training and Education, improving data recording and reporting processes.",
+      techStack: ["Codeigniter 13", "MySql"],
+      image: "/images/projects/sipb_fkip.png",
+      github: [
+         {
+            label: "Fullstack",
+            url: "https://github.com/mauluddin12z/SISTEM-INFORMASI-PERSEDIAAN-BARANG",
+         },
+      ],
+      liveDemo: "https://sipb-fkip.hidayatmauluddin.my.id",
+   },
+   {
+      title: "Score Tracker",
+      description:
+         "This project is a digital system designed to record, manage, and analyze student academic scores across different subjects and assessments. It enables teachers or administrators to input student grades, automatically calculate summaries such as averages, rankings, and performance trends, and generate meaningful insights about each student’s academic progress. The system aims to simplify academic monitoring by replacing manual record-keeping with an organized and efficient platform. It provides visual reports such as charts and dashboards to help identify high-performing students, students who need additional support, and overall class performance trends. Key features include secure student data management, score entry and updates, performance analytics, and report generation. This project helps educators make data-driven decisions to improve teaching strategies and enhance student learning outcomes.",
+      techStack: ["Vite React", "Express Js", "MySql"],
+      image: "/images/projects/score_tracker.png",
+      github: [
+         {
+            label: "Frontend",
+            url: "https://github.com/mauluddin12z/performance-analysis-frontend",
+         },
+         {
+            label: "Backend",
+            url: "https://github.com/mauluddin12z/performance-analysis-backend",
+         },
+      ],
+      liveDemo: "https://score-tracker.hidayatmauluddin.my.id",
+   },
 ];
